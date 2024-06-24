6f311f063c1e414606fff8b8cd43ad857c85128f
Divine Atah <d_atah@u.pacific.edu>
2024-06-24 11:40:49 +0100

    Created stock Dto and stock Mappers

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e65566ef3606f9e1c3e6aa92ea55559e93d3f0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07eaef3dcef37e14945c2f7355538c8f530f0ec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
