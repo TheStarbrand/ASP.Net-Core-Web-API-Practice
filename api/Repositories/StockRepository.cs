1e0499446c49f44a33ccc8a4bebdf0c67ddb1da0
Divine Atah <d_atah@u.pacific.edu>
2024-06-26 11:14:29 +0100

    modified stockrepo and its interface

--- api/Repositories/StockRepository.cs
+++ api/Repositories/StockRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
 using api.Interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -17,9 +18,38 @@ namespace api.Repositories
             _context = context;
         }
 
+        public async Task<Stock> CreateAsync(Stock stockModel)
+        {
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (stockModel == null){
+                return null;
+            }
+            _context.Stocks.Remove(stockModel);
+            await _context.SaveChangesAsync();            
+            return stockModel;
+        }
+
         public Task<List<Stock>> GetAllAsync()
         {
             return _context.Stocks.ToListAsync();
+        }
+
+        public Task<Stock?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock> UpdateAsync(int id, UpdateStockRequestDto stockDto)
+        {
+            throw new NotImplementedException();
         }
     }
 }
